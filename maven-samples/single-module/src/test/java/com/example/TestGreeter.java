79fd65621b291bf839e527dd8c82d211a1435db4
ismailinci <ismailinci265@gmail.com>
2023-05-02 04:44:41 +0300

    deneme

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,11 +41,4 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
-    @Test
-  public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, you re really suck";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
 }
